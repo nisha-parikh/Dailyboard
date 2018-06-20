ce00753bfbc144077ab020e20999ea787a76ac79
nisha-parikh <nishaparikh71@gmail.com>
2018-06-20 17:14:24 +0530

    fgh

---
+++ WindowsAppAssociates/Form1.Designer.cs
@@ -0,0 +1,362 @@
+﻿namespace WindowsAppAssociates
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.txtFloor = new System.Windows.Forms.TextBox();
+            this.txtCaseNumber = new System.Windows.Forms.TextBox();
+            this.txtCaseType = new System.Windows.Forms.TextBox();
+            this.txtClientName = new System.Windows.Forms.TextBox();
+            this.txtCourt = new System.Windows.Forms.TextBox();
+            this.ClientType = new System.Windows.Forms.ComboBox();
+            this.Insert = new System.Windows.Forms.Button();
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            this.View = new System.Windows.Forms.Button();
+            this.ViewDateRecord = new System.Windows.Forms.DateTimePicker();
+            this.printDocument1 = new System.Drawing.Printing.PrintDocument();
+            this.Print = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.txtLastDate = new System.Windows.Forms.DateTimePicker();
+            this.label5 = new System.Windows.Forms.Label();
+            this.label6 = new System.Windows.Forms.Label();
+            this.label7 = new System.Windows.Forms.Label();
+            this.label8 = new System.Windows.Forms.Label();
+            this.label9 = new System.Windows.Forms.Label();
+            this.label10 = new System.Windows.Forms.Label();
+            this.label11 = new System.Windows.Forms.Label();
+            this.Update = new System.Windows.Forms.Button();
+            this.label12 = new System.Windows.Forms.Label();
+            this.txtNewDate = new System.Windows.Forms.DateTimePicker();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // txtFloor
+            // 
+            this.txtFloor.Location = new System.Drawing.Point(133, 132);
+            this.txtFloor.Name = "txtFloor";
+            this.txtFloor.Size = new System.Drawing.Size(100, 20);
+            this.txtFloor.TabIndex = 0;
+            // 
+            // txtCaseNumber
+            // 
+            this.txtCaseNumber.Location = new System.Drawing.Point(552, 170);
+            this.txtCaseNumber.Name = "txtCaseNumber";
+            this.txtCaseNumber.Size = new System.Drawing.Size(100, 20);
+            this.txtCaseNumber.TabIndex = 1;
+            // 
+            // txtCaseType
+            // 
+            this.txtCaseType.Location = new System.Drawing.Point(552, 134);
+            this.txtCaseType.Name = "txtCaseType";
+            this.txtCaseType.Size = new System.Drawing.Size(100, 20);
+            this.txtCaseType.TabIndex = 2;
+            // 
+            // txtClientName
+            // 
+            this.txtClientName.Location = new System.Drawing.Point(133, 210);
+            this.txtClientName.Name = "txtClientName";
+            this.txtClientName.Size = new System.Drawing.Size(121, 20);
+            this.txtClientName.TabIndex = 3;
+            // 
+            // txtCourt
+            // 
+            this.txtCourt.Location = new System.Drawing.Point(133, 170);
+            this.txtCourt.Name = "txtCourt";
+            this.txtCourt.Size = new System.Drawing.Size(121, 20);
+            this.txtCourt.TabIndex = 4;
+            // 
+            // ClientType
+            // 
+            this.ClientType.FormattingEnabled = true;
+            this.ClientType.Location = new System.Drawing.Point(133, 247);
+            this.ClientType.Name = "ClientType";
+            this.ClientType.Size = new System.Drawing.Size(121, 21);
+            this.ClientType.TabIndex = 6;
+            // 
+            // Insert
+            // 
+            this.Insert.BackColor = System.Drawing.SystemColors.ActiveBorder;
+            this.Insert.Location = new System.Drawing.Point(621, 291);
+            this.Insert.Name = "Insert";
+            this.Insert.Size = new System.Drawing.Size(75, 23);
+            this.Insert.TabIndex = 7;
+            this.Insert.Text = "Insert";
+            this.Insert.UseVisualStyleBackColor = false;
+            this.Insert.Click += new System.EventHandler(this.Insert_Click);
+            // 
+            // dataGridView1
+            // 
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.Location = new System.Drawing.Point(11, 399);
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.Size = new System.Drawing.Size(844, 196);
+            this.dataGridView1.TabIndex = 8;
+            this.dataGridView1.CellContentClick += new System.Windows.Forms.DataGridViewCellEventHandler(this.dataGridView1_CellContentClick);
+            // 
+            // View
+            // 
+            this.View.Location = new System.Drawing.Point(359, 364);
+            this.View.Name = "View";
+            this.View.Size = new System.Drawing.Size(75, 23);
+            this.View.TabIndex = 9;
+            this.View.Text = "View";
+            this.View.UseVisualStyleBackColor = true;
+            this.View.Click += new System.EventHandler(this.View_Click);
+            // 
+            // ViewDateRecord
+            // 
+            this.ViewDateRecord.Location = new System.Drawing.Point(133, 366);
+            this.ViewDateRecord.Name = "ViewDateRecord";
+            this.ViewDateRecord.Size = new System.Drawing.Size(200, 20);
+            this.ViewDateRecord.TabIndex = 10;
+            // 
+            // printDocument1
+            // 
+            this.printDocument1.PrintPage += new System.Drawing.Printing.PrintPageEventHandler(this.printDocument1_PrintPage);
+            // 
+            // Print
+            // 
+            this.Print.Location = new System.Drawing.Point(780, 610);
+            this.Print.Name = "Print";
+            this.Print.Size = new System.Drawing.Size(75, 23);
+            this.Print.TabIndex = 11;
+            this.Print.Text = "Print";
+            this.Print.UseVisualStyleBackColor = true;
+            this.Print.Click += new System.EventHandler(this.Print_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(34, 137);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(60, 13);
+            this.label1.TabIndex = 12;
+            this.label1.Text = "Floor/Wing";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(59, 175);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(32, 13);
+            this.label2.TabIndex = 13;
+            this.label2.Text = "Court";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(59, 213);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(35, 13);
+            this.label3.TabIndex = 14;
+            this.label3.Text = "Name";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(34, 251);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(60, 13);
+            this.label4.TabIndex = 15;
+            this.label4.Text = "Client Type";
+            // 
+            // txtLastDate
+            // 
+            this.txtLastDate.Location = new System.Drawing.Point(552, 210);
+            this.txtLastDate.Name = "txtLastDate";
+            this.txtLastDate.Size = new System.Drawing.Size(200, 20);
+            this.txtLastDate.TabIndex = 16;
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(444, 137);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(58, 13);
+            this.label5.TabIndex = 17;
+            this.label5.Text = "Case Type";
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Location = new System.Drawing.Point(432, 175);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(71, 13);
+            this.label6.TabIndex = 18;
+            this.label6.Text = "Case Number";
+            // 
+            // label7
+            // 
+            this.label7.AutoSize = true;
+            this.label7.Location = new System.Drawing.Point(450, 213);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(53, 13);
+            this.label7.TabIndex = 19;
+            this.label7.Text = "Last Date";
+            // 
+            // label8
+            // 
+            this.label8.AutoSize = true;
+            this.label8.Location = new System.Drawing.Point(34, 372);
+            this.label8.Name = "label8";
+            this.label8.Size = new System.Drawing.Size(63, 13);
+            this.label8.TabIndex = 20;
+            this.label8.Text = "Select Date";
+            // 
+            // label9
+            // 
+            this.label9.AutoSize = true;
+            this.label9.Font = new System.Drawing.Font("Microsoft Sans Serif", 14F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label9.Location = new System.Drawing.Point(375, 323);
+            this.label9.Name = "label9";
+            this.label9.Size = new System.Drawing.Size(128, 24);
+            this.label9.TabIndex = 21;
+            this.label9.Text = "View Records";
+            // 
+            // label10
+            // 
+            this.label10.AutoSize = true;
+            this.label10.Font = new System.Drawing.Font("Times New Roman", 20.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label10.Location = new System.Drawing.Point(365, 70);
+            this.label10.Name = "label10";
+            this.label10.Size = new System.Drawing.Size(150, 31);
+            this.label10.TabIndex = 22;
+            this.label10.Text = "DailyBoard";
+            // 
+            // label11
+            // 
+            this.label11.AutoSize = true;
+            this.label11.Font = new System.Drawing.Font("Times New Roman", 20F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label11.Location = new System.Drawing.Point(300, 23);
+            this.label11.Name = "label11";
+            this.label11.Size = new System.Drawing.Size(301, 31);
+            this.label11.TabIndex = 23;
+            this.label11.Text = "S.D Patel and Associates";
+            // 
+            // Update
+            // 
+            this.Update.BackColor = System.Drawing.SystemColors.ActiveCaption;
+            this.Update.Location = new System.Drawing.Point(719, 291);
+            this.Update.Name = "Update";
+            this.Update.Size = new System.Drawing.Size(75, 23);
+            this.Update.TabIndex = 24;
+            this.Update.Text = "Update";
+            this.Update.UseVisualStyleBackColor = false;
+            this.Update.Click += new System.EventHandler(this.Update_Click);
+            // 
+            // label12
+            // 
+            this.label12.AutoSize = true;
+            this.label12.Location = new System.Drawing.Point(450, 251);
+            this.label12.Name = "label12";
+            this.label12.Size = new System.Drawing.Size(55, 13);
+            this.label12.TabIndex = 25;
+            this.label12.Text = "New Date";
+            // 
+            // txtNewDate
+            // 
+            this.txtNewDate.Location = new System.Drawing.Point(552, 245);
+            this.txtNewDate.Name = "txtNewDate";
+            this.txtNewDate.Size = new System.Drawing.Size(200, 20);
+            this.txtNewDate.TabIndex = 26;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(867, 645);
+            this.Controls.Add(this.txtNewDate);
+            this.Controls.Add(this.label12);
+            this.Controls.Add(this.Update);
+            this.Controls.Add(this.label11);
+            this.Controls.Add(this.label10);
+            this.Controls.Add(this.label9);
+            this.Controls.Add(this.label8);
+            this.Controls.Add(this.label7);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.txtLastDate);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.Print);
+            this.Controls.Add(this.ViewDateRecord);
+            this.Controls.Add(this.View);
+            this.Controls.Add(this.dataGridView1);
+            this.Controls.Add(this.Insert);
+            this.Controls.Add(this.ClientType);
+            this.Controls.Add(this.txtCourt);
+            this.Controls.Add(this.txtClientName);
+            this.Controls.Add(this.txtCaseType);
+            this.Controls.Add(this.txtCaseNumber);
+            this.Controls.Add(this.txtFloor);
+            this.Name = "Form1";
+            this.Text = "Dailyboard of SD Patel & Associates";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox txtFloor;
+        private System.Windows.Forms.TextBox txtCaseNumber;
+        private System.Windows.Forms.TextBox txtCaseType;
+        private System.Windows.Forms.TextBox txtClientName;
+        private System.Windows.Forms.TextBox txtCourt;
+        private System.Windows.Forms.ComboBox ClientType;
+        private System.Windows.Forms.Button Insert;
+        private System.Windows.Forms.DataGridView dataGridView1;
+        private System.Windows.Forms.Button View;
+        private System.Windows.Forms.DateTimePicker ViewDateRecord;
+        private System.Drawing.Printing.PrintDocument printDocument1;
+        private System.Windows.Forms.Button Print;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.DateTimePicker txtLastDate;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.Label label7;
+        private System.Windows.Forms.Label label8;
+        private System.Windows.Forms.Label label9;
+        private System.Windows.Forms.Label label10;
+        private System.Windows.Forms.Label label11;
+        private System.Windows.Forms.Button Update;
+        private System.Windows.Forms.Label label12;
+        private System.Windows.Forms.DateTimePicker txtNewDate;
+    }
+}
+
---
+++ WindowsAppAssociates/Form1.cs
@@ -0,0 +1,107 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Data.OleDb;
+
+namespace WindowsAppAssociates
+{
+    public partial class Form1 : Form
+    {
+        
+        OleDbConnection con = new OleDbConnection(@"Provider=Microsoft.ACE.OLEDB.12.0;Data Source=C:\Users\Nisha Parikh\Desktop\DailyBoard.accdb");
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Insert_Click(object sender, EventArgs e)
+        {
+            con.Open();
+            OleDbCommand cmd = con.CreateCommand();
+            cmd.CommandType = CommandType.Text;
+            cmd.CommandText = "Insert into dailyBoard (Floor,Court,Client_Name,Client_Type,Case_Type,Case_Number,Last_Date,New_Date) values('" + txtFloor.Text + "','" + txtCourt.Text + "','" + txtClientName.Text + "','" + ClientType.SelectedItem.ToString() + "','" + txtCaseType.Text + "','" + txtCaseNumber.Text + "','" + txtLastDate.Value.Date.ToString() + "','"+txtNewDate.Value.Date.ToString()+"')";
+            cmd.ExecuteNonQuery();
+            con.Close();
+            MessageBox.Show("Record Inserted");
+        }
+
+        private void View_Click(object sender, EventArgs e)
+        {
+            con.Open();
+            OleDbCommand cmd = con.CreateCommand();
+            cmd.CommandType = CommandType.Text;
+         
+            cmd.CommandText = "select * from dailyBoard where New_Date=#"+ ViewDateRecord.Value.Date.ToString() + "#";
+            cmd.ExecuteNonQuery();
+            DataTable dt = new DataTable();
+            OleDbDataAdapter da = new OleDbDataAdapter(cmd);
+            da.Fill(dt);
+            dataGridView1.DataSource = dt;
+            con.Close();
+           
+        }
+
+        private void Print_Click(object sender, EventArgs e)
+        {
+            printDocument1.Print();
+
+        }
+
+        private void printDocument1_PrintPage(object sender, System.Drawing.Printing.PrintPageEventArgs e)
+        {
+            Bitmap bm = new Bitmap(this.dataGridView1.Width, this.dataGridView1.Height);
+            dataGridView1.DrawToBitmap(bm, new Rectangle(0, 0, this.dataGridView1.Width, this.dataGridView1.Height));
+            e.Graphics.DrawImage(bm, 10, 10);
+         
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            ClientType.Items.Add("Plaintiff");
+            ClientType.Items.Add("Applicant");
+            ClientType.Items.Add("Petitioner");
+            ClientType.Items.Add("Complainant");
+            ClientType.Items.Add("Defendant");
+            ClientType.Items.Add("Opponent");
+            ClientType.Items.Add("Respondent");
+            ClientType.Items.Add("Accused");
+        }
+
+        private void dataGridView1_CellContentClick(object sender, DataGridViewCellEventArgs e)
+        {
+            if (e.RowIndex >= 0)
+            {
+                DataGridViewRow row = this.dataGridView1.Rows[e.RowIndex];
+
+                txtFloor.Text = row.Cells["Floor"].Value.ToString();
+                txtCourt.Text = row.Cells["Court"].Value.ToString();
+                txtClientName.Text = row.Cells["Client_Name"].Value.ToString();
+                ClientType.SelectedText = row.Cells["Client_Type"].Value.ToString();
+                txtCaseType.Text = row.Cells["Case_Type"].Value.ToString();
+                txtCaseNumber.Text = row.Cells["Case_Number"].Value.ToString();
+                txtLastDate.Value =Convert.ToDateTime(row.Cells["Last_Date"].Value.ToString());
+                txtNewDate.Value=Convert.ToDateTime(row.Cells["New_Date"].Value.ToString());
+
+            }
+        }
+
+        private void Update_Click(object sender, EventArgs e)
+        {
+            con.Open();
+            OleDbCommand cmd = con.CreateCommand();
+            cmd.CommandType = CommandType.Text;
+            cmd.CommandText = "Update dailyBoard SET Last_Date=#" + txtLastDate.Value.Date.ToString() + "#,Next_Date=#"+ txtNewDate.Value.Date.ToString() + "# where Case_Number='"+txtCaseNumber.Text+"'";
+            cmd.ExecuteNonQuery();
+            con.Close();
+
+            MessageBox.Show("Record Updated");
+        }
+    }
+}
